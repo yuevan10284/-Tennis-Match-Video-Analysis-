29f1dd91db02f4e94f983a2d6de52b304f9ff01c
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2024-03-26 03:06:25 -0700

    Delete Tennis-Match-Analysis directory

--- Tennis-Match-Analysis/yolo_inference.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO 
-
-model = YOLO('yolov8x')
-
-result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
-# print(result)
-# print("boxes:")
-# for box in result[0].boxes:
-#     print(box)
